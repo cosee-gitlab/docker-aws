2697336312c79f243f657f638c52ff08b5c137e2
Jannis Schaefer <j.schaefer@estwx.de>
2017-05-30 16:33:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Docker Image with awscli installed.
---
+++ build.sh
@@ -0,0 +1,9 @@
+docker build --pull \
+        -t $CONTAINER_IMAGE_NAME:latest \
+        -t $CONTAINER_IMAGE_NAME:17.05 \
+        -f Dockerfile.1705 .
+
+docker build --pull \
+        -t $CONTAINER_IMAGE_NAME:stable \
+        -t $CONTAINER_IMAGE_NAME:17.03 \
+        -f Dockerfile.1703 .
