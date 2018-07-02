f4205050c95349018f1c107049d00292033ca0bb
markus.zimmermann <markus.zimmermann@cosee.biz>
2018-07-02 18:13:57 +0200

    add latest stable and edge versions

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 docker build --pull \
         -t $CONTAINER_IMAGE_NAME:latest \
         -t $CONTAINER_IMAGE_NAME:edge \
+        -t $CONTAINER_IMAGE_NAME:18.05 \
+        -f Dockerfile.1805 .
+
+docker build --pull \
+        -t $CONTAINER_IMAGE_NAME:stable \
+        -t $CONTAINER_IMAGE_NAME:18.03 \
+        -f Dockerfile.1803 .
+
+docker build --pull \
         -t $CONTAINER_IMAGE_NAME:17.06 \
         -f Dockerfile.1706 .
 
@@ -9,6 +18,5 @@ docker build --pull \
         -f Dockerfile.1705 .
 
 docker build --pull \
-        -t $CONTAINER_IMAGE_NAME:stable \
         -t $CONTAINER_IMAGE_NAME:17.03 \
         -f Dockerfile.1703 .
